be30e3848e4047f80f9d3e2b99877c1e47207821
N-drakhman <fdrakhman@gmail.com>
2024-10-15 20:06:11 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+import ChildComponent from "./components/ChildComponent.vue";
+
+const childComponent = ref();
+</script>
+
+<template>
+  <button @click="childComponent.show = !childComponent.show">
+    Open section in parent
+  </button>
+  <ChildComponent ref="childComponent"></ChildComponent>
+</template>
+
+<style scoped></style>
---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from "vue";
+
+const show = ref(true);
+
+defineExpose({ show });
+</script>
+
+<template>
+    <button @click="show = !show">Open Section in component</button>
+    <Transition>
+      <div class="board" v-if="show" ref="board"></div>
+    </Transition>
+</template>
+
+<style>
+.board {
+  width: 90%;
+  height: 40px;
+  background-color: darkgray;
+  border-radius: 4px;
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+</style>
